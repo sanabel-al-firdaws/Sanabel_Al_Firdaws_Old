55e5c1bee49feefd275c3b91a8fd2a4acaf3737c
abdullhakim.sami <abdullhakim.sami@gmail.com>
2023-12-16 20:27:44 +0300

    testing out

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,6 +8,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+  trailingSlash: false,
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
